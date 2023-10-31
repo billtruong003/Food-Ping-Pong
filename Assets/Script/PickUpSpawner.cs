d79bb2bc9fefb973bd7e1ceddab0bb2526410d1f
BillTruong <billtruong003@gmail.com>
2023-10-31 09:05:20 +0700

    * Update

--- Assets/Script/PickUpSpawner.cs
+++ Assets/Script/PickUpSpawner.cs
@@ -15,25 +15,26 @@ public class PickUpSpawner : MonoBehaviour
 
     public void SpawnPickUp(int timeSpawn)
     {
-        StartCoroutine(Cor_SpawnPickUp(timeSpawn));
+        if (CheatSpawn)
+            timeSpawn = NumberSpawnCheat;
+        for (int i = 0; i < timeSpawn; i++)
+        {
+            StartCoroutine(Cor_SpawnPickUp(timeSpawn));
+        }
     }
 
     private IEnumerator Cor_SpawnPickUp(int numSpawn)
     {
         yield return new WaitUntil(() => GameManager.Instance != null);
-        if (CheatSpawn)
-            numSpawn = NumberSpawnCheat;
 
-        for (int i = 0; i < numSpawn; i++)
-        {
-            positionX = Random.Range(LimitTopLeft.x, LimitDownRight.x);
-            positionY = Random.Range(LimitTopLeft.y, LimitDownRight.y);
-            GameObject pickUp = GameManager.Instance.GetRawMaterial();
-            Instantiate(pickUp, PickUpContainer.GetChild(0));
-            pickUp.transform.localScale = Vector3.one;
-            pickUp.transform.localPosition = new Vector3(positionX, positionY);
-            yield return new WaitForSeconds(0.5f);
-        }
+        positionX = Random.Range(LimitTopLeft.x, LimitDownRight.x);
+        positionY = Random.Range(LimitTopLeft.y, LimitDownRight.y);
+        GameObject pickUp = GameManager.Instance.GetRawMaterial();
+        Instantiate(pickUp, PickUpContainer.GetChild(0));
+        pickUp.transform.localScale = Vector3.one;
+        pickUp.transform.localPosition = new Vector3(positionX, positionY);
+        yield return new WaitForSeconds(0.5f);
+
         GameManager.Instance.InitState(0);
     }
 }
