2bde978024fbd355beeee55bc0600edd00f4b561
bill <billtruong003@gmail.com>
2024-09-12 23:51:46 +0700

    *ok linhtinh

--- Assets/Script/FruitCode.cs
+++ Assets/Script/FruitCode.cs
@@ -2,18 +2,51 @@ using UnityEngine;
 
 public class FruitCode : MonoBehaviour
 {
+    #region properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+    //properties
+
+    #endregion
+
+    #region Variables
     [SerializeField] private string dayLaCodeCuaSora;
     [SerializeField] private string dayLaCodeCuaND;
+    // Important 
+    [SerializeField] private bool skipTut;
+
+    #endregion
+
+
+    #region 
+    private void Start()
+    {
+        if (skipTut)
+        {
+
+        }
+    }
+    #endregion
 
     public void HamCuaND()
     {
         dayLaCodeCuaSora = "sora";
     }
-    // Code code cac thu....
-    // Code code cac thu....
-    // Code code cac thu....
-    // Code code cac thu....
-    // Code code cac thu....
-    // Code code cac thu....
 
 }
